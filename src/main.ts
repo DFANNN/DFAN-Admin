012d72a7cef1a8e227ce7858cd7f3f22c27bcbd3
DFANNN <737812379@qq.com>
2025-08-05 15:22:54 +0800

    添加tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import '@/assets/common.css'
 import router from '@/router/index'
 import App from './App.vue'
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="login-container">
-    <div class="left">左</div>
-    <div class="right">
-      <routerView />
+  <div class="flex h-screen relative">
+    <div class="absolute top-4 left-4 flex items-center">
+      <img src="@/assets/vite.svg" alt="logo" class="w-10 mr-2" />
+      <p class="text-xl font-medium">Vite</p>
+    </div>
+    <div class="absolute top-4 right-4 flex items-center bg-gray-200 p-2 rounded-3xl">
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+    </div>
+    <div></div>
+    <div class="flex-1 bg-gray-100"></div>
+    <div class="w-1/3">
+      右
+      <!-- <routerView /> -->
     </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="less">
-.login-container {
-  display: flex;
-  .left {
-    flex: 1;
-  }
-  .right {
-    width: 30%;
-  }
-}
-</style>
+<style scoped lang="less"></style>
